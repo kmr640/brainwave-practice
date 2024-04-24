5dde26dc4b7b2ff611cad821b6fc799939cdaf8c
kmr640 <abckevin.ai@onmail.com>
2024-04-24 17:09:25 +0200

    intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react"
-
+import ButtonGradient from "./assets/svg/ButtonGradient"
+import Button from "./components/Button"
 const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <h1>Hi There!</h1>
+      <ButtonGradient />
+      <div className="">
+        <Button>Button</Button>
+      </div>
+    </>
+  )
 }
 
 export default App
