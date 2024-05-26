763eecd38e176687ef2e5fa3e10975c13bebdf8f
kmr640 <abckevin.ai@onmail.com>
2024-05-26 17:32:07 +0200

    init practice 5/26

---
+++ src/components/Section.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Section = ({
+  className,
+  id,
+  crosses,
+  crossesOffset,
+  customPaddings,
+  children,
+}) => {
+  return <div>Section</div>
+}
+
+export default Section
